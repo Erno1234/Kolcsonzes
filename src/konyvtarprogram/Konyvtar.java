5bdfe8808c46a336f1cc0533aebcafd597729d7b
sw4q3ar2 <biggercsgo@gmail.com>
2023-12-06 13:38:33 +0100

    termekek

--- src/konyvtarprogram/Konyvtar.java
+++ src/konyvtarprogram/Konyvtar.java
@@ -2,5 +2,23 @@
 package konyvtarprogram;
 
 public class Konyvtar {
+    private AbstractTermek[] termekek;
+
     
+    public void kolcsonozheto(){
+        for (int i = 0; i < termekek.length; i++) {
+            AbstractTermek aktTermek = this.termekek[i];
+            
+        }
+    }
+    
+    public void felvesz(){
+       termekek = new AbstractTermek[4];
+       termekek[0] = new Kave("fekete");
+       termekek[1] = new Konyv("Forest");
+       termekek[2] = new Konyv("Alma élete");
+       termekek[3] = new Kave("fekete");
+        
+    }
+
 }
--- src/konyvtarprogram/KonyvtarProgram.java
+++ src/konyvtarprogram/KonyvtarProgram.java
@@ -4,7 +4,7 @@ package konyvtarprogram;
 public class KonyvtarProgram {
 
     public static void main(String[] args) {
-        
+        new Konyvtar();
     }
     
 }
